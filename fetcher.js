948c901d4ae223b10a128013a159c18742dbaa71
MustafaSKaya <mustafakya88@hotmail.com>
2022-04-24 16:03:21 -0400

    modified again

--- fetcher.js
+++ fetcher.js
@@ -1,16 +1,23 @@
 const fs = require('fs');
-
+const { get } = require('https');
 const request = require('request');
+const args = process.argv.slice(2);
 
-request('http://www.reddit.com', (error, response, body) => {
+function getFilesizeInBytes(filename) {
+  let stats = fs.statSync(filename);
+  let fileSizeInBytes = stats.size;
+  return fileSizeInBytes;
+}
+
+request(args[0], (error, response, body) => {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   //console.log('body:', body);
-  fs.writeFile('./index.html', body, err => {
+  fs.writeFile(args[1], body, err => {
     if (err) {
       console.error(err)
       return
     }
-    console.log("file saved");
+    console.log(`Downloaded and saved ${getFilesizeInBytes("index.html")} of bytes in ${args[1]}.`);
   });
 });
